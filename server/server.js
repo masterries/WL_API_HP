88acd1ba6990cb9b502c5c6a7df6e59155b1ddb9
Patrick Ries <patrick@takting.com>
2024-05-30 05:23:39 +0200

    reverse

--- server/server.js
+++ server/server.js
@@ -8,26 +8,16 @@ const API_TARGET = 'https://m.qando.at';
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.get('/proxy', async (req, res) => {
     const url = API_TARGET + req.query.url;
     try {
-        const apiResponse = await fetch(url, {
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            }
-        });
+        const apiResponse = await fetch(url);
         const data = await apiResponse.json();
         res.json(data);
     } catch (error) {
-        console.error(error);
         res.status(500).json({ error: 'Error fetching data' });
     }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
